fb9212ab8768e0ca1f5d0b7f693bcfd331fa4fcf
Joseph Sumis <Jsumis@comcast.net>
2021-06-15 00:13:54 -0500

    created employee role js files and un-nedted prompts to index, created package.json, added inquirer node

--- index.js
+++ index.js
@@ -1,2 +1,124 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+const employee = require('./lib/employee.js');
+const manager = require('./lib/manager.js');
+const engineer = require('./lib/engineer.js');
+const intern = require('./lib/intern.js');
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+
+// Manager Prompts
+inquirer
+  .prompt([
+    {
+        type: 'input',
+        message: "What is the team manager's name?",
+        name: 'name',
+      },
+      {
+        type: 'input',
+        message: "What is the team manager's ID?",
+        name: 'id',
+      },
+    {
+      type: 'input',
+      message: "What is the team manager's email?",
+      name: 'email',
+    },
+    {
+        type: 'input',
+        message: "What is the team manager's office number?",
+        name: 'office',
+      },
+      {
+        type: 'list',
+        name: 'employee',
+        message: "Which type of team member would you like to add?",
+        choices: ["Engineer", "Intern", "I don't want to add any more team members"],
+      },
+     
+  ])
+
+  // Engineer Prompts
+  inquirer
+  .prompt([
+    {
+        type: 'input',
+        message: "What is the engineer's name?",
+        name: 'name',
+      },
+      {
+        type: 'input',
+        message: "What is the engineer's ID?",
+        name: 'id',
+      },
+    {
+      type: 'input',
+      message: "What is the engineer's email?",
+      name: 'email',
+    },
+    {
+        type: 'input',
+        message: "What is the engineer's GitHub username?",
+        name: 'github',
+      },
+      {
+        type: 'list',
+        name: 'employee',
+        message: "Which type of team member would you like to add?",
+        choices: ["Engineer", "Intern", "I don't want to add any more team members"],
+      },
+     
+  ])
+
+  // Engineer Prompts
+  inquirer
+  .prompt([
+    {
+        type: 'input',
+        message: "What is the intern's name?",
+        name: 'name',
+      },
+      {
+        type: 'input',
+        message: "What is the intern's ID?",
+        name: 'id',
+      },
+    {
+      type: 'input',
+      message: "What is the intern's email?",
+      name: 'email',
+    },
+    {
+        type: 'input',
+        message: "What is the intern's school?",
+        name: 'school',
+      },
+      {
+        type: 'list',
+        name: 'employee',
+        message: "Which type of team member would you like to add?",
+        choices: ["Engineer", "Intern", "I don't want to add any more team members"],
+      },
+     
+  ])
+
+
+// TODO: Create a function to write README file
+
+.then((data) => {
+const readMeContent = generateMarkdown(data);
+
+fs.writeFile('README.md', readMeContent, (err) =>
+  err ? console.error(err) : console.log('You successfully created a README file!')
+);
+});
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
