53dc97c3cd568a6a5c2f972e7c3e41a690198ecb
Joseph Sumis <Jsumis@comcast.net>
2021-06-15 16:34:10 -0500

    updated file structure

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+// npm dependencies
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+// 
 const Employee = require('./lib/Employee.js');
 const Manager = require('./lib/Manager.js');
 const Engineer = require('./lib/Engineer.js');
 const Intern = require('./lib/Intern.js');
+
+function addTeamMember ()
 
 // TODO: Create an array of questions for user input
 const questions = [];
