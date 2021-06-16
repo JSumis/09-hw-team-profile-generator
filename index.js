0fc0c07612205bb89c1ecd7a0cdc220046d7d15d
Joseph Sumis <Jsumis@comcast.net>
2021-06-15 22:28:25 -0500

    revised questions, added employeeRole functions, added starter html create function

--- index.js
+++ index.js
@@ -3,19 +3,21 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 // 
-const Employee = require('./lib/Employee.js');
-const Manager = require('./lib/Manager.js');
-const Engineer = require('./lib/Engineer.js');
-const Intern = require('./lib/Intern.js');
+// const Employee = require('./lib/Employee.js');
+// const Manager = require('./lib/Manager.js');
+// const Engineer = require('./lib/Engineer.js');
+// const Intern = require('./lib/Intern.js');
 
-function addTeamMember ()
+// function addTeamMember () {
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// }
 
+// // TODO: Create an array of questions for user input
+// const questions = [];
 
-// Manager Prompts
-inquirer
+const managerQuestions = () => {
+  
+  inquirer
   .prompt([
     {
         type: 'input',
@@ -36,17 +38,42 @@ inquirer
         type: 'input',
         message: "What is the team manager's office number?",
         name: 'office',
-      },
-      {
-        type: 'list',
-        name: 'employee',
-        message: "Which type of team member would you like to add?",
-        choices: ["Engineer", "Intern", "I don't want to add any more team members"],
-      },
-     
-  ])
+      }
+    ]) .then ((managerData) => {
 
-  // Engineer Prompts
+      console.log(managerData)
+      employeeRole()
+    })
+}
+
+const employeeRole = () => {
+
+  inquirer
+  .prompt([
+    {
+      type: 'list',
+      name: 'employee',
+      message: "Which type of team member would you like to add?",
+      choices: ["Engineer", "Intern", "I don't want to add any more team members"],
+    },
+  ]) .then ((employeeRoleData) => {
+    console.log(employeeRoleData)
+    if (employeeRoleData.employee === "Engineer") {
+      engineerQuestions()
+    }
+      else if (employeeRoleData.employee === "Intern") {
+        internQuestions()
+      }
+      else {
+        console.log("This is where we create html page")
+        createHtmlPage()
+      }
+    
+
+  })
+}
+
+const engineerQuestions = () => {
   inquirer
   .prompt([
     {
@@ -68,17 +95,14 @@ inquirer
         type: 'input',
         message: "What is the engineer's GitHub username?",
         name: 'github',
-      },
-      {
-        type: 'list',
-        name: 'employee',
-        message: "Which type of team member would you like to add?",
-        choices: ["Engineer", "Intern", "I don't want to add any more team members"],
-      },
-     
-  ])
+      }
+    ]) .then ((engineerData) => {
+      console.log(engineerData)
+      employeeRole()
+    })
+}
 
-  // Engineer Prompts
+const internQuestions = () => {
   inquirer
   .prompt([
     {
@@ -100,27 +124,49 @@ inquirer
         type: 'input',
         message: "What is the intern's school?",
         name: 'school',
-      },
-      {
-        type: 'list',
-        name: 'employee',
-        message: "Which type of team member would you like to add?",
-        choices: ["Engineer", "Intern", "I don't want to add any more team members"],
-      },
+      }
+    ]) .then ((internData) => {
+      console.log(internData)
+      employeeRole()
+    })
+}
+  
+    managerQuestions()
      
-  ])
+ 
 
+    const generateHTML = (answers) =>
+    `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <title>Document</title>
+  </head>
+  <body>
+    <div class="jumbotron jumbotron-fluid">
+    <div class="container">
+      <h1 class="display-4">Hi! My name is ${answers.name}</h1>
+      <p class="lead">I am from ${answers.location}.</p>
+      <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
+      <ul class="list-group">
+        <li class="list-group-item">My GitHub username is ${answers.github}</li>
+        <li class="list-group-item">LinkedIn: ${answers.linkedin}</li>
+      </ul>
+    </div>
+  </div>
+  </body>
+  </html>`;
 
-// TODO: Create a function to write README file
+const createHtmlPage = () => {
 
-.then((data) => {
-const readMeContent = generateMarkdown(data);
+    const htmlPageContent = generateHTML(answers);
 
-fs.writeFile('README.md', readMeContent, (err) =>
-  err ? console.error(err) : console.log('You successfully created a README file!')
-);
-});
-
+    fs.writeFile('index.html', htmlPageContent, (err) =>
+      err ? console.log(err) : console.log('Successfully created index.html!')
+    );
+  }
 // TODO: Create a function to initialize app
 function init() {}
 
